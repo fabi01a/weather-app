d6ef59ec56f5a3cef3da312bf855e62b6c247fa9
Fabi01a <gonzalezfabs@gmail.com>
2026-03-17 19:43:07 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import InputButton from './components/InputButton';
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Weather Report</h1>
+      <input />
+      <InputButton />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/InputButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function InputButton() {
+    return(
+        <section>
+            <button>SEARCH</button>
+        </section>
+    );
+};
+
+
+
+export default InputButton;
