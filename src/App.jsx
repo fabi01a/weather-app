b4c6faefbb2259c9af52ae607b1dacc5d332de08
Fabi01a <gonzalezfabs@gmail.com>
2026-04-16 16:45:58 -0700

    added current geolocation and permission prompt, refactored fetch call into its own function fetchWeather

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import InputButton from './components/InputButton';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import.meta.env.VITE_API_KEY
 
@@ -10,41 +10,61 @@ function App() {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
+  function fetchWeather(url) {
+    setLoading(true)
+    setError(null)
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.cod !== 200) {
+        setError("City not found")
+      } else {
+        setWeatherData(data)
+      }
+    })
+    .catch(() => {
+      setError("Something went wrong - please try again")
+    })
+    .finally( () => {
+      setLoading(false)
+    });
+  }
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=imperial`;
+        fetchWeather(url);
+      },
+      () => {
+        setError("Location access denied. Please search for a city");
+      }
+    );
+  }, []);
+
+
   function handleChange(e) {
     setSearchInput(e.target.value);
   }
 
   function handleClick() {
-    setError(null)
-    
     if (!searchInput.trim()){
       setError("Please enter a valid city")
       return
     }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${import.meta.env.VITE_API_KEY}&units=imperial`;
     
-    setLoading(true)
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${import.meta.env.VITE_API_KEY}&units=imperial`
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        if (data.cod !== 200) {
-          setError("City not found")
-        } else {
-          setWeatherData(data)
-        }
-      })
-      .catch(() => {
-        setError("Something went wrong - please try again")
-      })
-      .finally( () => {
-        setLoading(false)
-      })
+    fetchWeather(url);
   }
+
     return (
       <>
         <h1>Weather Report</h1>
-        <input value={searchInput} onChange={handleChange}/>
+        <input value={searchInput} onChange={handleChange} />
         <InputButton onClick={handleClick}>SEARCH</InputButton>
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
