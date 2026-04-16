0eca4485ac9d6ff13b9f0f268a66a89685a4647d
Fabi01a <gonzalezfabs@gmail.com>
2026-04-15 17:08:43 -0700

    added loading state, error city state

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,43 @@ import.meta.env.VITE_API_KEY
 function App() {
   const [searchInput, setSearchInput] = useState("")
   const [weatherData, setWeatherData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   function handleChange(e) {
     setSearchInput(e.target.value);
   }
   function handleClick() {
+    setError(null)
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${import.meta.env.VITE_API_KEY}&units=imperial`
+    setLoading(true)
     fetch(url)
       .then(response => response.json())
-      .then(data => setWeatherData(data));
+      .then(data => {
+        if (data.cod !== 200) {
+          setError("City not found")
+        } else {
+          setWeatherData(data)
+        }
+        setLoading(false)
+      });
   }
     return (
-      <div>
+      <>
         <h1>Weather Report</h1>
         <input value={searchInput} onChange={handleChange}/>
         <InputButton onClick={handleClick}>SEARCH</InputButton>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
         {weatherData && weatherData.weather && (
-          <div>
+          <>
             <p>{weatherData.name}</p>
             <p>{weatherData.main.temp}</p>
             <p>{weatherData.weather[0].main}</p>
             <p>{weatherData.weather[0].description}</p>
-            </div>
+          </>
         )}
-      </div>
+      </>
     );
   }
 
